491b0b63530a7a350b23254d4c936755f8104e06
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-30 12:29:06 -0300

    Feat:Adicionando Exceção para criação da basket

--- src/main/java/org/example/basketservice/client/PlatziStoreClient.java
+++ src/main/java/org/example/basketservice/client/PlatziStoreClient.java
@@ -1,12 +1,13 @@
 package org.example.basketservice.client;
 
 import org.example.basketservice.client.response.PlatziProductResponse;
+import org.example.basketservice.exceptions.customerErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "PlatziStoreClient", url ="${basket.client.platzi}" )
+@FeignClient(name = "PlatziStoreClient", url ="${basket.client.platzi}", configuration = {customerErrorDecoder.class})
 public interface PlatziStoreClient {
 
     @GetMapping("/products/{id}")
