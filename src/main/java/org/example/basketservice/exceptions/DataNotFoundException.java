4e2e75434dc953f6582cc7829f0288fe9f583724
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-30 12:31:45 -0300

    Feat:Criando exceção para se a basket não for encontrada

--- src/main/java/org/example/basketservice/exceptions/DataNotFoundException.java
+++ src/main/java/org/example/basketservice/exceptions/DataNotFoundException.java
@@ -1,4 +1,7 @@
 package org.example.basketservice.exceptions;
 
-public class DataNotFoundException {
+public class DataNotFoundException extends RuntimeException{
+    public DataNotFoundException(String message) {
+        super(message);
+    }
 }
