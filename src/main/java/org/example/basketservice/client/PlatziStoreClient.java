42071a442b03a7b594c58176de50cc1bf6edb1e8
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-30 09:35:42 -0300

    Feat: Adicionando o caminho da api e as requisições dos objetos que serão utilizados

---
+++ src/main/java/org/example/basketservice/client/PlatziStoreClient.java
@@ -0,0 +1,17 @@
+package org.example.basketservice.client;
+
+import org.example.basketservice.client.response.PlatziProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "PlatziStoreClient", url ="${basket.client.platzi}" )
+public interface PlatziStoreClient {
+
+    @GetMapping("/products/{id}")
+    PlatziProductResponse getProductsById(Long id);
+
+    @GetMapping("/products")
+    List<PlatziProductResponse> getAllProducts();
+}
