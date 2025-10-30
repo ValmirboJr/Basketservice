a456fbe9d4c712ee6e3102b4092ff32321aa136c
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-30 12:39:42 -0300

    Feat:Criação de endpoints de Delete,Update e forma de pagamento

--- src/main/java/org/example/basketservice/controller/BasketController.java
+++ src/main/java/org/example/basketservice/controller/BasketController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.basketservice.entity.Basket;
 import org.example.basketservice.repository.BasketRepository;
 import org.example.basketservice.request.BasketRequest;
+import org.example.basketservice.request.PaymentRequest;
 import org.example.basketservice.service.BasketService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,26 @@ public class BasketController {
 
     @PostMapping
     public ResponseEntity<Basket> save(@RequestBody BasketRequest request) {
-        return  ResponseEntity.status(HttpStatus.CREATED).body(basketService.CreateBasket(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(basketService.CreateBasket(request));
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Basket> getBasketById(@PathVariable String id) {
         return ResponseEntity.ok(basketService.getBasketById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Basket> update(@PathVariable String id, @RequestBody BasketRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(basketService.update(id, request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Basket> payBasket(@PathVariable String id, @RequestBody PaymentRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(basketService.payBasket(id, request));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBasket(@PathVariable String id) {
+        basketService.deleteBasket(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
